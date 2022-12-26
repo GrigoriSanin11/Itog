db680475172c7dd7049f157ad4a97b9ecb8c4f7c
Dom <96082010@mail.ru>
2022-12-26 20:36:22 +0300

    002

---
+++ DZ/Program.cs
@@ -0,0 +1,20 @@
+﻿String [] znach= new string [7] {"sadf", "adadfs", "afafds","afsdgwe","wfwevs","ar","a"};
+String [] znach1= new string [znach.Length];
+
+
+void main (string znach, string znach1)
+{
+    int count = 0;
+     for (int i = 0; i < znach.Length; i++)
+    {
+        if (znach.Length <= 3 )
+            {
+                znach[i] = znach1[count];
+                count++;
+
+            }
+    }
+
+
+
+}
